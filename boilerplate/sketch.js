a01aee566843e2c2b96c592827903fa7f86d9170
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-20 20:33:36 +0200

    Updated Boilerplate. Added Corefunctions.

--- boilerplate/sketch.js
+++ boilerplate/sketch.js
@@ -1,28 +1,25 @@
-// some probably useful and often needed variables
 const container = document.getElementById('p5-container')
-// make sure there is a #p5-container in index.html. 
+// make sure there is a #p5-container in index.html.
 
+// Canvas Vars
 let canW = container.offsetWidth //canvas Width
 let canH = container.offsetHeight //canvas Height
 let canMax = Math.max(canW, canH) //longer canvas side
 let canMin = Math.min(canW, canH) //shorter canvas side
 
+// p5 Setup
 function setup() {
     //initial setup of canvas and container
     let canvas = createCanvas(canW,canH)
     canvas.parent(container)
 
     //actual code starts here
-    background('rgba(0, 255, 0, 1)')    
+    background(0,255,0)
+    if (coinToss(50)) {
+        drawGuides(5,10)
+    }
 }
 
+// p5 Draw
 function draw() {
-
-}
-
-// doesn't always work
-function windowResized() {
-    resizeCanvas(canW, canH)
-    canW = container.offsetWidth
-    canH = container.offsetHeight
 }
