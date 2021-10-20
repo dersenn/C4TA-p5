3ec67e68930b3a1ebcffc7b7334abbc61f7cb13f
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-20 19:42:17 +0200

    All Together Now! :-)

--- 211020-cos-its-a-sin-2/sketch.js
+++ 211020-cos-its-a-sin-2/sketch.js
@@ -9,14 +9,16 @@ let canMax = Math.max(canW, canH) //longer canvas side
 let canMin = Math.min(canW, canH) //shorter canvas side
 
 // Global Vars
-let nPoints = 12
-let maxA
-let a
-let r
+let a = 0
+let aMax
+let nSteps = 180
+let aStep
+let r = canMin/10
+let d = 2 * r
 
-let prevX
-let prevY
+let monochrome = false //true for all white circles
 
+// p5 Setup
 function setup() {
     //initial setup of canvas and containing container (sic!)
     let canvas = createCanvas(canW,canH)
@@ -24,30 +26,73 @@ function setup() {
 
     //actual code starts here
     frameRate(60)
-    maxA = TAU
-    a = maxA / nPoints
+    aMax = PI
+    aStep = aMax / nSteps
 }
 
+function drawCircle(x,y,dia,color) {
+    noStroke()
+    if (monochrome) {
+        fill(255)
+    } else {
+        fill(color.r,color.g,color.b)
+    }
+    ellipse(x,y,dia)
+}
+
+// p5 Setup
 function draw() {
     translate(width/2,height/2)
-    background(255,255,255)
-    for (let p = 0; p < nPoints; p++) {
-        r = random(250,250)
-        if (prevX == undefined && prevY == undefined) {
-            prevX = sin(p * a) * r
-            prevY = cos(p * a) * r
-        }
-        pX = sin(p * a) * r
-        pY = cos(p * a) * r
-        line(prevX,prevY,pX,pY)
-        noFill()
-        stroke(0,255,0)
-        line(0,0,pX,pY)
-        stroke(0)
-        ellipse(pX,pY,10)
-        prevX = pX
-        prevY = pY
+    background(0,0,0)
 
-        a += maxA / 36000
-    }
+    let sine = sin(a)
+    let cosine = cos(a)
+
+    let sinX = map(sine,-1,1,-width/2 + r,width/2 - r)
+    let cosX = map(cosine,-1,1,-width/2 + r,width/2 - r)
+    let sinY = map(sine,-1,1,-height/2 + r,height/2 - r)
+    let cosY = map(cosine,-1,1,-height/2 + r,height/2 - r)
+
+    // blue. left/right.
+    drawCircle(sinX,0,d,{r: 0, g: 0, b: 255})
+    drawCircle(-sinX,0,d,{r: 0, g: 0, b: 255})
+
+    drawCircle(cosX,0,d,{r: 0, g: 0, b: 128})
+    drawCircle(-cosX,0,d,{r: 0, g: 0, b: 128})
+
+
+    // green. up/down.
+    drawCircle(0,sinY,d,{r: 0, g: 255, b: 0})
+    drawCircle(0,-sinY,d,{r: 0, g: 255, b: 0})
+
+    drawCircle(0,cosY,d,{r: 0, g: 128, b: 0})
+    drawCircle(0,-cosY,d,{r: 0, g: 128, b: 0})
+
+    // red. circular
+    drawCircle(sinX,cosY,d,{r: 255, g: 0, b: 0})
+    drawCircle(-sinX,cosY,d,{r: 255, g: 0, b: 0})
+    drawCircle(sinX,-cosY,d,{r: 255, g: 0, b: 0})
+    drawCircle(-sinX,-cosY,d,{r: 255, g: 0, b: 0})
+
+    drawCircle(cosX,sinY,d,{r: 128, g: 0, b: 0})
+    drawCircle(-cosX,sinY,d,{r: 128, g: 0, b: 0})
+    drawCircle(cosX,-sinY,d,{r: 128, g: 0, b: 0})
+    drawCircle(-cosX,-sinY,d,{r: 128, g: 0, b: 0})
+
+    // yellow. diagonal, top left/bottom right
+    drawCircle(sinX,sinY,d,{r: 255, g: 255, b: 0})
+    drawCircle(cosX,cosY,d,{r: 255, g: 255, b: 0})
+
+    drawCircle(-sinX,-sinY,d,{r: 128, g: 128, b: 0})
+    drawCircle(-cosX,-cosY,d,{r: 128, g: 128, b: 0})
+
+    // magenta. diagonal, bottom left/top right
+    drawCircle(cosX,-cosY,d,{r: 255, g: 0, b: 255})
+    drawCircle(sinX,-sinY,d,{r: 255, g: 0, b: 255})
+
+    drawCircle(-cosX,cosY,d,{r: 128, g: 0, b: 128})
+    drawCircle(-sinX,sinY,d,{r: 128, g: 0, b: 128})
+
+
+    a += aStep
 }
