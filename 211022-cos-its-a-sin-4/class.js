e7f0c63b4421acb81b1f5f8123dd3a5a45ab8d7a
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-22 21:16:26 +0200

    Switches are switching.

--- 211022-cos-its-a-sin-4/class.js
+++ 211022-cos-its-a-sin-4/class.js
@@ -1,13 +1,34 @@
-class Xyz {
-    constructor(something) {
-        this.something = something
+class Switch {
+    constructor(x,y,w,h,aInit) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        if (this.h > this.w) {
+            this.isportrait = true
+            this.dia = this.w
+        } else {
+            this.dia = this.h
+        }
+        this.r = this.dia / 2
+
+        this.aInit = aInit
+        this.a = random() * TAU
     }
-    classFunction() {
-        //do stuff
+    Draw() {
+        fill(0)
+        rect(this.x,this.y,this.w,this.h)
+
+        if (this.isportrait) {
+            this.cX = this.x + this.r
+            this.cY = map(cos(this.a),-1,1,this.y + this.r, this.y + this.h - this.r)
+        } else {
+            this.cX = map(sin(this.a),-1,1,this.x + this.r,this.x + this.w - this.r)
+            this.cY = this.y + this.r
+        }
+
+        fill(255)
+        ellipse(this.cX, this.cY,this.dia)
+        this.a += .05
     }
 }
-
-// use like so (in main app)
-// let v = new Xyz(something)
-// v.classFunction()
-
--- 211022-cos-its-a-sin-4/sketch.js
+++ 211022-cos-its-a-sin-4/sketch.js
@@ -7,6 +7,12 @@ let canH = container.offsetHeight //canvas Height
 let canMax = Math.max(canW, canH) //longer canvas side
 let canMin = Math.min(canW, canH) //shorter canvas side
 
+// Global Vars
+let thing
+let things = [] 
+
+// Functions
+
 // p5 Setup
 function setup() {
     //initial setup of canvas and container
@@ -14,12 +20,17 @@ function setup() {
     canvas.parent(container)
 
     //actual code starts here
-    background(0,255,0)
-    if (coinToss(50)) {
-        drawGuides(5,10)
+    thing = new Switch(0,0,canW,200,0)
+    for (let i = 0; i < 4; i++) {
+        let step = width / 4
+        things.push( new Switch(i * step,200,step,canH - 200,0))
     }
 }
 
 // p5 Draw
 function draw() {
+    thing.Draw()
+    for (let t = 0; t < things.length; t++) {
+        things[t].Draw()
+    }
 }
