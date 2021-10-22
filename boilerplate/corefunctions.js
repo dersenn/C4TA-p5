0f054e496269047eee2ac2833d6078acb8e646a6
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-22 16:08:29 +0200

    Added shuffleArray to corefunctions.

--- boilerplate/corefunctions.js
+++ boilerplate/corefunctions.js
@@ -26,6 +26,23 @@ function drawGuides(cols,rows) {
     pop()
 }
 
+// Shuffle Array (Fisher Yates Shuffle)
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+// https://bost.ocks.org/mike/shuffle/
+function shuffleArray(array) {
+  let currentIndex = array.length,  randomIndex;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
 // doesn't work
 // don't know if it ever did... needs some work.
 function windowResized() {
---
+++ snippets.js
@@ -0,0 +1,31 @@
+// array stuff
+
+let myArray = []
+
+myArray.push("Rot") // Add to End of Array
+myArray.unshift("Blau") // Add to Start of Array
+myArray.pop() // Remove from End of Array
+myArray.shift() // Remove from Start of Array
+
+myArray.reverse() // Reverse Array
+
+// Shuffle Array (Fisher Yates Shuffle)
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+// https://bost.ocks.org/mike/shuffle/
+
+function shuffleArray(array) {
+  let currentIndex = array.length,  randomIndex;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
+//endless array loop
+
