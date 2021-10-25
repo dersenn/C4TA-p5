129350aa0d008f5d3685e032188153bbd6c5e007
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-25 08:54:03 +0200

    basic "flag" movement.

--- 211025-cos-its-a-sin-5/class.js
+++ 211025-cos-its-a-sin-5/class.js
@@ -1,13 +1,32 @@
-class Xyz {
-    constructor(something) {
-        this.something = something
+class Tile {
+    constructor(posX, posY, tW, tH, aInit) {
+        this.x = posX
+        this.y = posY
+        this.w = tW
+        this.h = tH
+        this.cX = this.x + this.w/2
+        this.cY = this.y + this.h/2
+
+        this.a = aInit
+        this.r = this.w / 6
+        this.d = this.r * 2
+
+        this.fgCol = random(255)
+        this.bgCol = 255 - this.fgCol
     }
-    classFunction() {
-        //do stuff
+    draw() {
+        push()
+
+        translate(this.cX, this.cY)
+
+        this.pX = sin(this.a) * (this.w/2 - this.r)
+        this.pY = cos(this.a) * (this.h/2 - this.r)
+
+        fill(0)
+        ellipse(this.pX, this.pY,this.d)
+
+        pop()
+
+        this.a += 0.05
     }
 }
-
-// use like so (in main app)
-// let v = new Xyz(something)
-// v.classFunction()
-
--- 211025-cos-its-a-sin-5/sketch.js
+++ 211025-cos-its-a-sin-5/sketch.js
@@ -7,6 +7,47 @@ let canH = container.offsetHeight //canvas Height
 let canMax = Math.max(canW, canH) //longer canvas side
 let canMin = Math.min(canW, canH) //shorter canvas side
 
+
+// Global Vars
+let tiles = []
+let cols = 9
+let rows = cols
+let nTiles = cols * rows
+
+
+// Functions
+function makeGrid(zeroX, zeroY, gridCols, gridRows, gridW, gridH, initLevel, maxLevel, arr) {
+    let tileW = gridW / gridCols
+    let tileH = gridH / gridRows
+
+    let myTiles = arr
+    let aInit = 0
+    let aStep = TAU / nTiles
+
+    for (let x = 0; x < gridCols; x++) {
+        let xOff = zeroX + (x * tileW)
+
+            for (let y = 0; y < gridRows; y++) {
+                let yOff = zeroY + (y * tileH)
+
+                if (initLevel < maxLevel) {
+                    if (coinToss(50)) {
+                        myTiles.push( new Tile(xOff, yOff, tileW, tileH, aInit))
+                    } else {
+                        makeGrid(xOff, yOff, gridCols, gridRows, tileW, tileH, initLevel+1, maxLevel, myTiles)
+                    }
+                } else {
+                    myTiles.push( new Tile(xOff, yOff, tileW, tileH, aInit))
+                }
+
+                aInit += aStep
+
+            }
+    }
+    return myTiles
+}
+
+
 // p5 Setup
 function setup() {
     //initial setup of canvas and container
@@ -14,12 +55,13 @@ function setup() {
     canvas.parent(container)
 
     //actual code starts here
-    background(0,255,0)
-    if (coinToss(50)) {
-        drawGuides(5,10)
-    }
+    tiles = makeGrid(0,0,rows,cols,canW,canH,0,0,tiles)
 }
 
 // p5 Draw
 function draw() {
+    background(255)
+    for (let t = 0; t < tiles.length; t++) {
+        tiles[t].draw()
+    }
 }
