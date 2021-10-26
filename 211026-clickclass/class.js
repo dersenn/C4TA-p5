db6b3f8655fc1d82d75475c3ff7a860a1b120ea0
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-26 22:38:22 +0200

    changed pos to vector. tried something with the speed...

--- 211026-clickclass/class.js
+++ 211026-clickclass/class.js
@@ -1,13 +1,13 @@
 class Node {
     constructor(posX, posY) {
-        this.x = posX
-        this.y = posY
+        this.pos = createVector(posX,posY)
 
         // something's not working right with this speed thing (not so important)
-        let speedFactor = 1000
+        // hmm, the "radius" of the vector doesn't seem to be important for the speed...
+        let speedFactor = 100000
 
-        this.speedX = sin( ( random()*TAU ) * ( random()*speedFactor ) )
-        this.speedY = cos( ( random()*TAU ) * ( random()*speedFactor ) )
+        this.speedX = sin( ( random()*TAU ) * speedFactor )
+        this.speedY = cos( ( random()*TAU ) * speedFactor )
         this.speed = createVector( this.speedX, this.speedY )
 
         this.dia = random(10,100)
@@ -15,31 +15,31 @@ class Node {
     }
 
     updatePos() {
-        this.x += this.speed.x
-        this.y += this.speed.y
+        this.pos.x += this.speed.x
+        this.pos.y += this.speed.y
     }
 
     checkPos() {
-        if (this.x < 0 + this.r || this.x > width - this.r) {
+        if (this.pos.x < 0 + this.r || this.pos.x > width - this.r) {
             this.speed.x *= -1
         }
-        if (this.y < 0 + this.r || this.y > height - this.r) {
+        if (this.pos.y < 0 + this.r || this.pos.y > height - this.r) {
             this.speed.y *= -1
         }
     }
 
     drawNode() {
         fill(0)
-        ellipse(this.x, this.y, this.dia)
+        ellipse(this.pos.x, this.pos.y, this.dia)
         this.checkPos()
         this.updatePos()
     }
 
     drawNet(nodes) {
-        line(this.x, this.y, 0, 0)
-        line(this.x, this.y, 0, height)
-        line(this.x, this.y, width, height)
-        line(this.x, this.y, width, 0)
+        line(this.pos.x, this.pos.y, 0, 0)
+        line(this.pos.x, this.pos.y, 0, height)
+        line(this.pos.x, this.pos.y, width, height)
+        line(this.pos.x, this.pos.y, width, 0)
 
 
         // grid between nodes not working (maybe check some collision detection stuff)
