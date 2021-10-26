2f38e4917b16c8576b97175c29f58697d6fb8721
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-26 21:21:22 +0200

    basic click function and classes implemented. grid between nodes not working.

--- 211026-clickclass/class.js
+++ 211026-clickclass/class.js
@@ -1,13 +1,49 @@
-class Xyz {
-    constructor(something) {
-        this.something = something
+class Node {
+    constructor(posX, posY) {
+        this.x = posX
+        this.y = posY
+
+        // something's not working right with this speed thing (not so important)
+        let speedFactor = 1000
+
+        this.speedX = sin( ( random()*TAU ) * ( random()*speedFactor ) )
+        this.speedY = cos( ( random()*TAU ) * ( random()*speedFactor ) )
+        this.speed = createVector( this.speedX, this.speedY )
+
+        this.dia = random(10,100)
+        this.r = this.dia / 2
     }
-    classFunction() {
-        //do stuff
+
+    updatePos() {
+        this.x += this.speed.x
+        this.y += this.speed.y
+    }
+
+    checkPos() {
+        if (this.x < 0 + this.r || this.x > width - this.r) {
+            this.speed.x *= -1
+        }
+        if (this.y < 0 + this.r || this.y > height - this.r) {
+            this.speed.y *= -1
+        }
+    }
+
+    drawNode() {
+        fill(0)
+        ellipse(this.x, this.y, this.dia)
+    }
+
+    drawNet(nodes) {
+        line(this.x, this.y, 0, 0)
+        line(this.x, this.y, 0, height)
+        line(this.x, this.y, width, height)
+        line(this.x, this.y, width, 0)
+
+
+        // grid between nodes not working (maybe check some collision detection stuff)
+        // this.other = nodes
+        // for (let i = 0; i < this.other.length; i++) {
+        //     line(this.x, this.y, this.other.x, this.other.y)
+        // }
     }
 }
-
-// use like so (in main app)
-// let v = new Xyz(something)
-// v.classFunction()
-
--- 211026-clickclass/sketch.js
+++ 211026-clickclass/sketch.js
@@ -2,24 +2,39 @@ const container = document.getElementById('p5-container')
 // make sure there is a #p5-container in index.html.
 
 // Canvas Vars
+let canvas
 let canW = container.offsetWidth //canvas Width
 let canH = container.offsetHeight //canvas Height
 let canMax = Math.max(canW, canH) //longer canvas side
 let canMin = Math.min(canW, canH) //shorter canvas side
 
+
+let nodes = []
+
+
+function createNode() {
+    nodes.push( new Node( mouseX, mouseY ) )
+}
+
 // p5 Setup
 function setup() {
     //initial setup of canvas and container
-    let canvas = createCanvas(canW,canH)
+    canvas = createCanvas(canW,canH)
     canvas.parent(container)
 
     //actual code starts here
-    background(0,255,0)
-    if (coinToss(50)) {
-        drawGuides(5,10)
-    }
+    canvas.mouseClicked(createNode)
 }
 
 // p5 Draw
 function draw() {
+    background(255)
+    for (let n = 0; n < nodes.length; n++) {
+        let node = nodes[n]
+        node.checkPos()
+        node.updatePos()
+        node.drawNode()
+        node.drawNet(nodes)
+    }
+    // console.log(nodes)
 }
