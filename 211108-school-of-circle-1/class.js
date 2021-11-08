6c363a7c80df87c1fa5f522872eba2857441fb7c
dersenn <67318888+dersenn@users.noreply.github.com>
2021-11-08 11:20:32 +0100

    initial setup. abandoning for now.

--- 211108-school-of-circle-1/class.js
+++ 211108-school-of-circle-1/class.js
@@ -1,13 +1,54 @@
-class Xyz {
-    constructor(something) {
-        this.something = something
+class Grid {
+    constructor(zeroX, zeroY, w, h, cols, rows, subcols, subrows) {
+        this.pos = {x: zeroX, y: zeroY}
+        this.dim = {w: w, h: h}
+        this.cols = cols
+        this.rows = rows
+        this.sCols = subcols
+        this.sRows = subrows
+
+        this.tileW = this.dim.w / this.cols
+        this.tileH = this.dim.h / this.rows
+        this.tiles = []
     }
-    classFunction() {
-        //do stuff
-    }
+
+    makeGrid() {
+        for (let col = 0; col < this.cols; col++) {
+            this.colOff = this.pos.x + (col * this.tileW)
+
+                for (let row = 0; row < this.rows; row++) {
+                    this.rowOff = this.pos.y + (row * this.tileH)
+                    this.tiles.push( new Tile(this.colOff, this.rowOff, this.tileW, this.tileH) )
+                }
+
+        }
+
+        return this.tiles
+        }
 }
 
-// use like so (in main app)
-// let v = new Xyz(something)
-// v.classFunction()
 
+class Tile {
+    constructor(zeroX, zeroY, w, h) {
+        this.init = {x: zeroX, y: zeroY}
+        this.dim = {w: w, h: h}
+
+        this.angles = [QUARTER_PI, HALF_PI, PI]
+        this.a = 0
+
+    }
+
+    drawTile() {
+
+        let sine = sin(this.a) + 1
+        this.pos = {
+            x: map(sine, -1, 1, this.init.x, this.init.x + this.dim.w),
+            y: this.init.y + this.dim.h/2
+        }
+
+        ellipse(this.pos.x, this.pos.y, 10)
+
+        this.a += TAU/60
+
+    }
+}
--- 211108-school-of-circle-1/sketch.js
+++ 211108-school-of-circle-1/sketch.js
@@ -1,3 +1,6 @@
+// This:
+// https://twitter.com/satoshi_aizawa/status/1456240252029259788
+
 const container = document.getElementById('p5-container')
 // make sure there is a #p5-container in index.html.
 
@@ -7,19 +10,27 @@ let canH = container.offsetHeight //canvas Height
 let canMax = Math.max(canW, canH) //longer canvas side
 let canMin = Math.min(canW, canH) //shorter canvas side
 
+let maingrid
+let margin = canMin / 10
+
 // p5 Setup
 function setup() {
-    //initial setup of canvas and container
     let canvas = createCanvas(canW,canH)
     canvas.parent(container)
 
-    //actual code starts here
-    background(0,255,0)
-    if (coinToss(50)) {
-        drawGuides(5,10)
-    }
+    maingrid = new Grid(margin/2, margin/2, width - margin, height - margin, 3, 3, 12, 12)
+    maingrid.makeGrid()
+    console.log(maingrid)
 }
 
 // p5 Draw
 function draw() {
+    background(255)
+    let length = .75
+    let color = 255
+    for (let tile of maingrid.tiles) {
+        tile.drawTile()
+    length += .25
+    color -= 255 / maingrid.tiles.length + 1
+    }
 }
