4916c8f504d26c8b98bd9ee0698a99100c700ead
dersenn <67318888+dersenn@users.noreply.github.com>
2022-01-28 20:30:35 +0100

    Slightly random ;-)

--- 220128-3d-one/sketch.js
+++ 220128-3d-one/sketch.js
@@ -6,15 +6,41 @@ let canMax = Math.max(canW, canH) //longer canvas side
 let canMin = Math.min(canW, canH) //shorter canvas side
 
 // "Global" vars
+let nRows = 3
+let nCols = nRows
+let boxW = canMin / nRows
+let boxH = canMin / nCols
 
+let zero
 
 
 // p5 Setup
 function setup() {
-  let canvas = createCanvas(canW,canH)
+  let canvas = createCanvas(canW,canH,WEBGL)
   canvas.parent(container)
+  // the bull starts here
 }
 
 // p5 Draw
 function draw() {
+  background(0)
+  orbitControl()
+  noFill()
+  stroke(0,255,0)
+
+  zero = {
+    x: -(width/2) + boxW/2,
+    y: -(height/2) + boxH/2,
+    z: -(width/2) + boxW/2
+  }
+
+  for (let x = 0; x < nRows; x++) {
+    for (let y = 0; y < nCols; y++) {
+      push()
+      translate(zero.x + x*boxW, zero.y + y*boxH, zero.z * random())
+      box(boxW)
+      pop()
+    }
+  }
+
 }
